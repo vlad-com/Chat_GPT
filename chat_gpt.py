5027ab9deb1167fa4fa769bfd009d00b2305d018
vlad-com <jhdncybudw@proton.me>
2023-12-07 21:23:11 +0200

    add g4f, update openai to v1

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,56 +1,73 @@
 import os
 import openai
+from openai import AsyncOpenAI
+import g4f
 from config import OPENAI_API_KEY, MODEL
 
 openai.api_key = OPENAI_API_KEY
+client = AsyncOpenAI(api_key=OPENAI_API_KEY)
+
+
+async def g4f_generate_text(messages, userid):
+    return await g4f.ChatCompletion.create_async(
+        provider=g4f.Provider.GeekGpt,
+        model=MODEL,
+        messages=messages,
+    )
+
+
+async def g4f_generate_text_chunks(messages, userid):
+    return await g4f.ChatCompletion.create_async(
+        provider=g4f.Provider.GeekGpt,
+        model=MODEL,
+        messages=messages,
+        stream=True,
+    )
 
 
 async def generate_text(messages, userid):
-    completion = await openai.ChatCompletion.acreate(
+    completion = await client.chat.completions.create(
         model=MODEL,
         messages=messages,
+        user=str(userid),
         max_tokens=2500,
         temperature=0.7,
         frequency_penalty=0,
         presence_penalty=0,
-        user=userid
     )
-    return completion.choices[0]['message']['content']
+    return completion.choices[0].text
 
 
 async def generate_text_chunks(messages, userid):
-    return await openai.ChatCompletion.acreate(
+    return await client.chat.completions.create(
         model=MODEL,
         messages=messages,
-        max_tokens=2500,
-        temperature=0.7,
-        frequency_penalty=0,
-        presence_penalty=0,
-        user=userid,
         stream=True,
     )
 
 
 async def generate_image(prompt):
-    response = await openai.Image.acreate(
+    response = await client.images.generate(
+        model="dall-e-3",
         prompt=prompt,
-        n=1,
         size="512x512",
-        response_format="url",
+        quality="standard",
+        n=1,
     )
 
-    return response['data'][0]['url']
+    return response.data[0].url
 
 
 async def whisper(path):
     size = os.path.getsize(path)
     if size < 25000000:
         audio_file = open(path, "rb")
-        transcript = await openai.Audio.atranscribe(
-            model="whisper-1",
-            file=audio_file,
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file, 
+            response_format="text"
         )
-        return transcript["text"]
+        return transcript
     else:
         raise Exception("Input file is to large(>25 Mb)")
 
